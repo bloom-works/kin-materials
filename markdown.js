a1c12c45093fc14de9eb726a09f877741e004d77
Josh Rubenoff <1328849+jrubenoff@users.noreply.github.com>
2025-02-12 10:39:44 -0800

    Remove header links

--- markdown.js
+++ markdown.js
@@ -1,13 +1,10 @@
 const markdownIt = require('markdown-it');
-const markdownItAnchor = require('markdown-it-anchor');
 const attrs = require('markdown-it-attrs');
 
 const markdownLibrary = markdownIt({
   html: true,
   breaks: false,
   linkify: true
-}).use(markdownItAnchor, {
-  permalink: markdownItAnchor.permalink.headerLink()
 }).use(attrs);
 
 module.exports = markdownLibrary;
